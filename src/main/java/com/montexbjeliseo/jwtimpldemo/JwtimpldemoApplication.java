fe6568f4df7c69f6e5a594557e4403f7458b085a
montexbjeliseo <secondmtx@gmail.com>
2023-05-01 15:59:57 -0300

    Initial commit

---
+++ src/main/java/com/montexbjeliseo/jwtimpldemo/JwtimpldemoApplication.java
@@ -0,0 +1,13 @@
+package com.montexbjeliseo.jwtimpldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtimpldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtimpldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montexbjeliseo/jwtimpldemo/JwtimpldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montexbjeliseo.jwtimpldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtimpldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
